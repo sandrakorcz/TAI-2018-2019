08d469f948fedebdeaa086b20f658d84072182ed
Sandra Korcz <35773058+sandrakorcz@users.noreply.github.com>
2019-02-14 14:25:51 +0100

    Add files via upload

---
+++ PHP/6_typyDanych.php
@@ -0,0 +1,21 @@
+<?php
+	//$sentence="To jest zdanie";
+	//echo(int) $sentence;//zwróci 0
+	//$score= (int) 14.8;
+	//echo $score;
+
+    //$total=5;//liczba calkowita
+    //$count="15";//ciag znaków
+    //echo $total+=$count;//$total=20 liczba calkowita
+
+    //$total="45 silników";
+    //$incoming=10;
+    //echo $total=$incoming+$total;//$total=55
+
+    //$total="1,0";
+    //if($total) echo "Jestesmy w dobrym nastroju!";
+
+    $val1="1.2e3";//1,200
+    $val2=2;
+    echo $val1*$val2;//zwraca wynik 2400
+?>
