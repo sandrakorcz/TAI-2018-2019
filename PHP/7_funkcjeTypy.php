40bfa535c14e607d60db4077e8ba84c84a46d69c
Sandra Korcz <35773058+sandrakorcz@users.noreply.github.com>
2019-02-21 11:34:41 +0100

    Add files via upload

--- PHP/7_funkcjeTypy.php
+++ PHP/7_funkcjeTypy.php
@@ -1,6 +1,6 @@
 <?php
 $item=43;
-printf("Zmienna \$item jest typu array: %d </br >", is_array($item));
+printf("Zmienna $item jest typu array: %d </br >", is_array($item));
 printf("Zmienna \$item jest typu integer: %d </br >", is_integer($item));
 printf("Zmienna \$item jest liczb¹ (numeric): %d </br >", is_numeric($item));
 ?>
